48b33cb28460c5688d3bfa7b41f3f7bbceb7ef78
ShadowSecurity <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-04-14 04:43:51 -0500

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -3,10 +3,10 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const HTML_CACHE = "html";
-const JS_CACHE = "js";
-const STYLE_CACHE = "css";
-const IMAGE_CACHE = "assets";
-const FONT_CACHE = "webfonts";
+const JS_CACHE = "javascript";
+const STYLE_CACHE = "stylesheets";
+const IMAGE_CACHE = "images";
+const FONT_CACHE = "fonts";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
