75c412dcac18c20b47c3435dfc0878df194e56d6
CY83R-3X71NC710N <EOF09340902jgkjkgnrekhi49@protonmail.com>
2022-05-09 13:55:36 -0500

    fixrotation

--- js/fixrotation.js
+++ js/fixrotation.js
@@ -1,9 +1,4 @@
-<script type="text/javascript"> // RELOADS WEBPAGE WHEN MOBILE ORIENTATION CHANGES  
-    window.onorientationchange = function() { 
-        var orientation = window.orientation; 
-            switch(orientation) { 
-                case 0:
-                case 90:
-                case -90: window.location.reload(); 
-                break; } 
-    };
+window.onorientationchange = function()
+{
+   window.location.reload();
+}
