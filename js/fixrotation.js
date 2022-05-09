b0b92e6c45590d48ea9021385f754cf1a23e84f3
CY83R-3X71NC710N <EOF09340902jgkjkgnrekhi49@protonmail.com>
2022-05-09 13:57:43 -0500

    fixrotation

--- js/fixrotation.js
+++ js/fixrotation.js
@@ -1,4 +1,7 @@
-window.onorientationchange = function()
-{
-   window.location.reload();
-}
+window.addEventListener('orientationchange', function () {
+    var originalBodyStyle = getComputedStyle(document.body).getPropertyValue('display');
+    document.body.style.display='none';
+    setTimeout(function () {
+      document.body.style.display = originalBodyStyle;
+    }, 10);
+  });
