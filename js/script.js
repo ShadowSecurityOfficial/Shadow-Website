390476c47887f83d2532aab9f83c9ec8a7264d76
CY83R-3X71NC710N <EOF09340902jgkjkgnrekhi49@protonmail.com>
2022-05-08 22:18:16 -0500

    Update Shadow-Website

--- js/script.js
+++ js/script.js
@@ -1,117 +1,281 @@
-// script-tutorials.com | @script-tutorials
-// Copyright (c) 2022 CY83R-3X71NC710N
+/*
+Copyright (c) 2022 by Chase Kaiser (https://codepen.io/chasekaiser/pen/ogXmOK)
+Copyright (c) 2022 CY83R-3X71NC710N
+*/
 
-// inner variables
-var canvas, ctx;
-var camera, scene, renderer, meshMaterial, mesh, geometry, i, f;
-var mouseX = 0, mouseY = 0;
-var startTime = new Date().getTime();
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
+var canvas = document.querySelector(".hacker-3d-shiz"),
+    ctx = canvas.getContext("2d"),
+    canvasBars = document.querySelector(".bars-and-stuff"),
+    ctxBars = canvasBars.getContext("2d"),
+    outputConsole = document.querySelector(".output-console");
 
-if (window.attachEvent) {
-    window.attachEvent('onload', main_init);
-} else {
-    if(window.onload) {
-        var curronload = window.onload;
-        var newonload = function() {
-            curronload();
-            main_init();
-        };
-        window.onload = newonload;
-    } else {
-        window.onload = main_init;
+canvas.width = (window.innerWidth/3)*2;
+canvas.height = window.innerHeight / 3;
+
+canvasBars.width = window.innerWidth/3;
+canvasBars.height = canvas.height;
+
+outputConsole.style.height = (window.innerHeight / 3) * 2 + 'px';
+outputConsole.style.top = window.innerHeight / 3 + 'px'
+
+
+/* Graphics stuff */
+function Square(z) {
+    this.width = canvas.width/2;
+    
+    if(canvas.height < 200){
+      this.width = 200;
+    };
+  
+    this.height = canvas.height;
+    z = z || 0;
+
+    this.points = [
+    new Point({
+        x: (canvas.width / 2) - this.width,
+        y: (canvas.height / 2) - this.height,
+        z: z
+    }),
+    new Point({
+        x: (canvas.width / 2) + this.width,
+        y: (canvas.height / 2) - this.height,
+        z: z
+    }),
+    new Point({
+        x: (canvas.width / 2) + this.width,
+        y: (canvas.height / 2) + this.height,
+        z: z
+    }),
+    new Point({
+        x: (canvas.width / 2) - this.width,
+        y: (canvas.height / 2) + this.height,
+        z: z
+    })];
+    this.dist = 0;
+}
+
+Square.prototype.update = function () {
+    for (var p = 0; p < this.points.length; p++) {
+        this.points[p].rotateZ(0.001);
+        this.points[p].z -= 3;
+        if (this.points[p].z < -300) {
+            this.points[p].z = 2700;
+        }
+        this.points[p].map2D();
     }
 }
 
-function main_init() {
+Square.prototype.render = function () {
+    ctx.beginPath();
+    ctx.moveTo(this.points[0].xPos, this.points[0].yPos);
+    for (var p = 1; p < this.points.length; p++) {
+        if (this.points[p].z > -(focal - 50)) {
+            ctx.lineTo(this.points[p].xPos, this.points[p].yPos);
+        }
+    }
 
-    // creating canvas and context objects
-    canvas = document.getElementById('panel');
-    var ctx = canvas.getContext('2d');
+    ctx.closePath();
+    ctx.stroke();
 
-    // preparing camera
-    camera = new THREE.Camera(30, window.innerWidth / window.innerHeight, 1, 5000);
-    camera.position.z = 6000;
+    this.dist = this.points[this.points.length - 1].z;
 
-    // preparing scene
-    scene = new THREE.Scene();
+};
 
-    // preparing geometry
-    geometry = new THREE.Geometry();
+function Point(pos) {
+    this.x = pos.x - canvas.width / 2 || 0;
+    this.y = pos.y - canvas.height / 2 || 0;
+    this.z = pos.z || 0;
 
-    // loading texture
-    var texture = THREE.ImageUtils.loadTexture('../assets/clouds.png');
-    texture.magFilter = THREE.LinearMipMapLinearFilter;
-    texture.minFilter = THREE.LinearMipMapLinearFilter;
+    this.cX = 0;
+    this.cY = 0;
+    this.cZ = 0;
 
-    // preparing fog
-    var fog = new THREE.Fog(0xFFFFFF, - 100, 50000);
+    this.xPos = 0;
+    this.yPos = 0;
+    this.map2D();
+}
 
-    // preparing material
-    meshMaterial = new THREE.MeshShaderMaterial({
-        uniforms: {
-            'map': {type: 't', value:2, texture: texture},
-            'fogColor' : {type: 'c', value: fog.color},
-            'fogNear' : {type: 'f', value: fog.near},
-            'fogFar' : {type: 'f', value: fog.far},
+Point.prototype.rotateZ = function (angleZ) {
+    var cosZ = Math.cos(angleZ),
+        sinZ = Math.sin(angleZ),
+        x1 = this.x * cosZ - this.y * sinZ,
+        y1 = this.y * cosZ + this.x * sinZ;
 
-        },
-        vertexShader: document.getElementById('vs').textContent,
-        fragmentShader: document.getElementById('fs').textContent,
-        depthTest: false
+    this.x = x1;
+    this.y = y1;
+}
+
+Point.prototype.map2D = function () {
+    var scaleX = focal / (focal + this.z + this.cZ),
+        scaleY = focal / (focal + this.z + this.cZ);
+
+    this.xPos = vpx + (this.cX + this.x) * scaleX;
+    this.yPos = vpy + (this.cY + this.y) * scaleY;
+};
+
+// Init graphics stuff
+var squares = [],
+    focal = canvas.width / 2,
+    vpx = canvas.width / 2,
+    vpy = canvas.height / 2,
+    barVals = [],
+    sineVal = 0;
+
+/* fake console stuff */
+var commandStart = ['Performing DNS Lookups for', 
+                'Searching ', 
+                'Analyzing ', 
+                'Estimating Approximate Location of ', 
+                'Compressing ', 
+                'Requesting Authorization From : ', 
+                'wget -a -t ', 
+                'tar -xzf ', 
+                'Entering Location ', 
+                'Compilation Started of ',
+                 'Downloading '],
+    commandParts = ['Data Structure', 
+                    'http://wwjd.com?au&2', 
+                    'Texture', 
+                    'TPS Reports', 
+                    ' .... Searching ... ', 
+                    'http://zanb.se/?23&88&far=2', 
+                    'http://ab.ret45-33/?timing=1ww'],
+    commandResponses = ['Authorizing ', 
+                 'Authorized...', 
+                 'Access Granted..', 
+                 'Going Deeper....', 
+                 'Compression Complete.', 
+                 'Compilation of Data Structures Complete..', 
+                 'Entering Security Console...', 
+                 'Encryption Unsuccesful Attempting Retry...', 
+                 'Waiting for response...', 
+                 '....Searching...', 
+                 'Calculating Space Requirements '
+                ],
+    isProcessing = false,
+    processTime = 0,
+    lastProcess = 0;
+
+
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    squares.sort(function (a, b) {
+        return b.dist - a.dist;
     });
+    for (var i = 0, len = squares.length; i < len; i++) {
+        squares[i].update();
+        squares[i].render();
+    }
+    
+    ctxBars.clearRect(0, 0, canvasBars.width, canvasBars.height);
+    
+    ctxBars.beginPath();
+    var y = canvasBars.height/6;
+    ctxBars.moveTo(0,y);
+    
+    for(i = 0; i < canvasBars.width; i++){
+        var ran = (Math.random()*20)-10;
+        if(Math.random() > 0.98){
+             ran = (Math.random()*50)-25   
+        }
+        ctxBars.lineTo(i, y + ran);
+    }
+    
+    ctxBars.stroke();
+    
+    for(i = 0; i < canvasBars.width; i+=20){
+        if(!barVals[i]){
+            barVals[i] = {
+                val : Math.random()*(canvasBars.height/2),
+                freq : 0.1,
+                sineVal : Math.random()*100
+            };
+        }
+        
+        barVals[i].sineVal+=barVals[i].freq;
+        barVals[i].val+=Math.sin(barVals[i].sineVal*Math.PI/2)*5;
+        ctxBars.fillRect(i+5,canvasBars.height,15,-barVals[i].val);
+    }
+    
+    requestAnimationFrame(render);
+}
 
-    // preparing planeMesh
-    var planeMesh = new THREE.Mesh(new THREE.PlaneGeometry(64, 64));
-    for (i = 0; i < 10000; i++) {
-        planeMesh.position.x = Math.random() * 1000 - 500;
-        planeMesh.position.y = - Math.random() * Math.random() * 200 - 15;
-        planeMesh.position.z = i;
-        planeMesh.rotation.z = Math.random() * Math.PI;
-        planeMesh.scale.x = planeMesh.scale.y = Math.random() * Math.random() * 1.5 + 0.5;
-
-        THREE.GeometryUtils.merge(geometry, planeMesh);
+function consoleOutput(){
+    var textEl = document.createElement('p');
+    
+    if(isProcessing){
+        textEl = document.createElement('span');
+        textEl.textContent += Math.random() + " ";
+        if(Date.now() > lastProcess + processTime){
+            isProcessing = false;   
+        }
+    }else{
+        var commandType = ~~(Math.random()*4);
+        switch(commandType){
+            case 0:
+                textEl.textContent = commandStart[~~(Math.random()*commandStart.length)] + commandParts[~~(Math.random()*commandParts.length)];
+                break;
+            case 3: 
+                isProcessing = true;
+                processTime = ~~(Math.random()*5000);
+                lastProcess = Date.now();
+            default:
+                 textEl.textContent = commandResponses[~~(Math.random()*commandResponses.length)];
+            break;
+        }
     }
 
-    mesh = new THREE.Mesh(geometry, meshMaterial);
-    scene.addObject(mesh);
-
-    mesh = new THREE.Mesh(geometry, meshMaterial);
-    mesh.position.z = - 10000;
-    scene.addObject(mesh);
-
-    // preparing new renderer and drawing it
-    renderer = new THREE.WebGLRenderer({ antialias: false });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    // change positions by mouse
-    document.addEventListener('mousemove', onMousemove, false);
-
-    // change canvas size on resize
-    window.addEventListener('resize', onResize, false);
-
-    setInterval(drawScene, 30); // loop drawScene
+    outputConsole.scrollTop = outputConsole.scrollHeight;
+    outputConsole.appendChild(textEl);
+    
+    if (outputConsole.scrollHeight > window.innerHeight) {
+       var removeNodes = outputConsole.querySelectorAll('*');
+       for(var n = 0; n < ~~(removeNodes.length/3); n++){
+            outputConsole.removeChild(removeNodes[n]);
+        }
+    }
+    
+    setTimeout(consoleOutput, ~~(Math.random()*200));
 }
 
-function onMousemove(event) {
-    mouseX = (event.clientX - windowHalfX) * 0.3;
-    mouseY = (event.clientY - windowHalfY) * 0.2;
-}
 
-function onResize(event) {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
+setTimeout(function(){   
+      canvas.width = (window.innerWidth/3)*2;
+      canvas.height = window.innerHeight / 3;
 
-function drawScene() {
-    position = ((new Date().getTime() - startTime) * 0.1) % 10000;
+      canvasBars.width = window.innerWidth/3;
+      canvasBars.height = canvas.height;
 
-    camera.position.x += mouseX * 0.01;
-    camera.position.y += - mouseY * 0.01;
-    camera.position.z = - position + 10000;
+      outputConsole.style.height = (window.innerHeight / 3) * 2 + 'px';
+      outputConsole.style.top = window.innerHeight / 3 + 'px';
+  
+      focal = canvas.width / 2;
+      vpx = canvas.width / 2;
+      vpy = canvas.height / 2;
 
-    renderer.render(scene, camera);
-}
+      for (var i = 0; i < 15; i++) {
+          squares.push(new Square(-300 + (i * 200)));
+      }
+  
+      ctx.strokeStyle = ctxBars.strokeStyle = ctxBars.fillStyle = '#FF0000';
+  
+      render();
+      consoleOutput();
+}, 200);
+
+window.addEventListener('resize', function(){
+      canvas.width = (window.innerWidth/3)*2;
+      canvas.height = window.innerHeight / 3;
+
+      canvasBars.width = window.innerWidth/3;
+      canvasBars.height = canvas.height;
+
+      outputConsole.style.height = (window.innerHeight / 3) * 2 + 'px';
+      outputConsole.style.top = window.innerHeight / 3 + 'px';
+  
+      focal = canvas.width / 2;
+      vpx = canvas.width / 2;
+      vpy = canvas.height / 2;
+      ctx.strokeStyle = ctxBars.strokeStyle = ctxBars.fillStyle = '#FF0000';
+});
