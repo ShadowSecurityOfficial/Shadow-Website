4d898abc1b115916845f6908c39fc3b8045f25a5
ShadowSecurity <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-05-10 16:27:56 -0500

    Update fixrotation.js

--- js/fixrotation.js
+++ js/fixrotation.js
@@ -2,3 +2,10 @@ window.onorientationchange = function()
 {
    window.location.reload();
 }
+
+window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+}
